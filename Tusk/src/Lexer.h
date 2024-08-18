7affaf20de81431d083341d62563955fe203f857
SteliosKem <stylianoskem@gmail.com>
2024-08-19 00:09:18 +0300

    Added Lexer files from 'Horizon' repo and refactored them

---
+++ Tusk/src/Lexer.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <array>
+#include "token.h"
+
+namespace Tusk {
+	class Lexer {
+	public:
+		Lexer(const std::string& _source) : m_source(_source) {}
+		Lexer() { m_source = ""; }
+
+		const std::vector<Token>& analyze();					// Lexes the source code and returns vector of tokens
+		bool had_error() const;									// If an error is produced from the lexer it is reported here
+
+		std::array<std::string, 12> keywords{ "return", "let", "fn", "void", "if", "else", "while", "do", "for", "break", "continue" };
+		//std::array<std::string, 15> types{ "isize", "fsize", "i8", "i16", "i32", "i64", "f32", "f64", "u8", "usize", "u16", "u32", "u64", "string", "void" };
+	private:
+		static bool is_digit(char character);					// Check if a character is a digit
+		static bool is_alpha(char character);					// Check if a character is alphanumeric
+
+		void next();											// Advances the index and updates current_char
+		void back();
+		bool match(char expected);								// Match next character
+
+		Token lex();											// Lex a single Token
+
+		Token string();											// Make string Token
+		Token number();											// Make number Token
+		Token identifier();										// Make identifier/keyword Token
+
+	private:
+		char m_current_char{ '\0' };							// Current character
+		int m_index{ -1 };										// Current index
+		int m_line{ 1 };										// Current line inside a file
+
+		std::string m_source{};									// The source code to lex
+
+		bool m_had_error{ false };
+
+		std::vector<Token> m_out;
+	};
+}
--- Tusk/src/Tusk.cpp
+++ Tusk/src/Tusk.cpp
@@ -1,20 +1,22 @@
-// Tusk.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "pch.h"
 #include <iostream>
+#include <string>
+#include "Lexer.h"
+
+// This file is for testing in production. It will be stripped out when this project becomes a library.
+
+using namespace Tusk;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string in;
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, in);
+        Lexer lexer(in);
+        const std::vector<Token>& tokens = lexer.analyze();
+        for (const Token& tok : tokens) {
+            std::cout << (int)tok.type << '\n';
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
